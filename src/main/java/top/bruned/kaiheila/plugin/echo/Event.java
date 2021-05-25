5917f4715c1dc12f95bf54adb36a8cb482dbd61e
bruned <2136407907@qq.com>
2021-05-25 13:52:27 +0800

    精简功能

--- src/main/java/top/bruned/kaiheila/plugin/echo/Event.java
+++ src/main/java/top/bruned/kaiheila/plugin/echo/Event.java
@@ -1,48 +1,26 @@
 package top.bruned.kaiheila.plugin.echo;
 
-import com.alibaba.fastjson.JSONObject;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import top.bruned.kaiheila.framework.plugin.EventClass;
 import top.bruned.kaiheila.framework.plugin.annotation.EventHandler;
 
-import top.bruned.kaiheila.framework.plugin.eventmethod.onGroupTextMessageEvent;
+import top.bruned.kaiheila.sdk.bot.api.message_create.message_create_data;
 import top.bruned.kaiheila.sdk.wsclient.result.event.GroupTextMessageEvent.GroupTextMessageEvent;
 
-import java.io.IOException;
-@EventHandler(recvBotMessage = false)
-public class Event extends EventClass implements onGroupTextMessageEvent{
-    public static final String baseUrl = "http://127.0.0.1:3000";
+
+@EventHandler(recvBotMessage = true)
+public class Event extends EventClass {
     @Override
     public void onEvent(GroupTextMessageEvent event){
-        if (event.getContent().startsWith("/echo ")&&!event.getContent().substring(6).trim().isEmpty()){
-            bot.api.Message_create(event.getTarget_id(), event.getContent().substring(6).trim());
+        if(event.getContent().startsWith("@bruned_bot#2901688749")){
+            String card = "[{\"type\":\"card\",\"theme\":\"secondary\",\"size\":\"lg\",\"modules\":[{\"type\":\"header\",\"text\":{\"type\":\"plain-text\",\"content\":\"信息查询结果\"}},{\"type\":\"section\",\"text\":{\"type\":\"plain-text\",\"content\":\"你的ID：%s\"}},{\"type\":\"section\",\"text\":{\"type\":\"plain-text\",\"content\":\"频道ID：%s\"}},{\"type\":\"section\",\"text\":{\"type\":\"plain-text\",\"content\":\"消息ID：%s\"}}]}]";
+            bot.message_create(new message_create_data(event.getTarget_id(),String.format(card,event.getAuthor_id(),event.getTarget_id(),event.getMsg_id())).setType(10));
         }
-        else if(event.getContent().startsWith("/song ")){
-            String data = getCard(event.getContent().substring(6).trim());
-            bot.api.Message_create(10,event.getTarget_id(),data);
+        else if (event.getContent().startsWith("/echo ")&&!event.getContent().substring(6).trim().isEmpty()){
+            bot.message_create(new message_create_data(event.getTarget_id(),event.getContent().substring(6).trim()));
+            bot.message_delete(event.getMsg_id());
         }
         else if(event.getContent().startsWith("/card ")){
-            bot.api.Message_create(10,event.getTarget_id(),event.getContent().substring(6).trim());
+            bot.message_create(new message_create_data(event.getTarget_id(),event.getContent().substring(6).trim()).setType(10));
         }
     }
-
-    public static String getCard(String name){
-        String card = "[{\\\"type\\\":\\\"card\\\",\\\"theme\\\":\\\"secondary\\\",\\\"size\\\":\\\"lg\\\",\\\"modules\\\":[{\\\"type\\\":\\\"audio\\\",\\\"title\\\":\\\"%s-%s\\\",\\\"src\\\":\\\"%s\\\",\\\"cover\\\":\\\"%s\\\"}]}]";
-        JSONObject song = get(String.format("/cloudsearch?keywords=%s&type=1&limit=1", name)).getJSONObject("result").getJSONArray("songs").getJSONObject(0);
-        return String.format(card,song.getString("name"),song.getJSONArray("ar").getJSONObject(0).getString("name"),get(String.format("/song/url?id=%s",song.getIntValue("id"))).getJSONArray("data").getJSONObject(0).getString("url"),song.getJSONObject("al").getString("picUrl"));
-    }
-
-    private static JSONObject get(String url){
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(baseUrl+url).get().build();
-        try (Response response = client.newCall(request).execute()) {
-            return JSONObject.parseObject(response.body().string());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new JSONObject();
-    }
-
 }
--- src/main/java/top/bruned/kaiheila/plugin/echo/Plugin.java
+++ src/main/java/top/bruned/kaiheila/plugin/echo/Plugin.java
@@ -2,15 +2,16 @@ package top.bruned.kaiheila.plugin.echo;
 
 import top.bruned.kaiheila.framework.plugin.JavaPlugin;
 import top.bruned.kaiheila.framework.plugin.annotation.PluginINFO;
+import top.bruned.kaiheila.sdk.bot.api.message_create.message_create_data;
 
 @PluginINFO(PluginName = "EchoPlugin",Version = "0.0.0",Author = "bruned")
-public class plugin extends JavaPlugin {
+public class Plugin extends JavaPlugin {
     @Override
     public void onEnable() {
         pluginConfig.getJsonObject().put("test","test");
         pluginConfig.saveConfig();
         log.info("测试插件启动");
-        bot.api.Message_create("1767539110347849","Bot上线");
+        bot.message_create(new message_create_data("1767539110347849","Bot上线"));
         registerEvent(new Event());
 
     }
